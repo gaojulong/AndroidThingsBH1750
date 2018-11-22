9617e329d0a10d63d1c910454b90f550af591a5d
gaojulong <49465154@qq.com>
2018-11-22 11:13:31 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/example/bkrc_109/mythingsbh1750/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.example.bkrc_109.mythingsbh1750;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.bkrc_109.mythingsmc5883", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/com/example/bkrc_109/mythingsbh1750/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.bkrc_109.mythingsbh1750;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import com.google.android.things.pio.I2cDevice;
+import com.google.android.things.pio.PeripheralManager;
+import java.io.IOException;
+
+public class MainActivity extends Activity {
+
+    private static final int INTERVAL_BETWEEN_READ_MS = 500;
+    // I2C Device Name
+    private static final String I2C_DEVICE_NAME = "I2C1";
+    // I2C Slave Address
+    private static final int I2C_ADDRESS = 0x23;
+    private I2cDevice i2cDevice;
+    private MyBH1750 mBH1750;
+
+
+
+//    private I2cDevice i2cDevice;
+    private static final String TAG = "MainActivity";
+    PeripheralManager manager = PeripheralManager.getInstance();
+    Handler handler =new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            i2cDevice = manager.openI2cDevice(I2C_DEVICE_NAME,I2C_ADDRESS);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mBH1750 = new MyBH1750(i2cDevice);
+        mBH1750.initSetting();
+        handler.post(mReadRunnable);
+    }
+
+
+    private Runnable mReadRunnable = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                float ill = mBH1750.readBH1750Data();
+                Log.d(TAG,"光照强度:"+(int)ill);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            handler.postDelayed(this,INTERVAL_BETWEEN_READ_MS);
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mBH1750.close();
+    }
+}
